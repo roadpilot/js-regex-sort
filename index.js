5166377336be41617d39735373a85f225dd85bd8
MartinAchterman <65299501+roadpilot@users.noreply.github.com>
2021-08-11 17:58:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+
+    var patt = /gmail.com/i
+    var result = []
+    for (let NItr = 0; NItr < N; NItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const firstName = firstMultipleInput[0];
+
+        const emailID = firstMultipleInput[1];
+        // console.log(emailID)
+        if (patt.test(emailID)){
+            result.push(firstName)            
+        }
+    }
+    result.sort().forEach(el => console.log(el))
+}
